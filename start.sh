f038073deaa03c4a842b0e7322a81b0a6f09fdc0
ashir <nadarashir1501@gmail.com>
2026-05-14 18:48:22 +0530

    Update: shell script and email config

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 python manage.py migrate --noinput
 
-celery -A job_board worker -l info --concurrency=1 &
+celery -A job_board worker -l info --pool=solo &
 
 exec gunicorn job_board.wsgi:application \
     --bind 0.0.0.0:$PORT \
