6a927d813a569a42ee4b4030324065473e43e4ba
ashir <nadarashir1501@gmail.com>
2026-05-14 11:02:31 +0530

    Updated: db config in base.py

--- job_board/settings/base.py
+++ job_board/settings/base.py
@@ -132,9 +132,9 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": env("DB_NAME"),
-            "USER": env("DB_USER"),
-            "PASSWORD": env("DB_PASSWORD"),
+            "NAME": env("DB_NAME", default='postgrdb'),
+            "USER": env("DB_USER",default='postgrpass'),
+            "PASSWORD": env("DB_PASSWORD",default='postgre@App'),
             "HOST": env("DB_HOST", default="db"),
             "PORT": env("DB_PORT", default="5432"),
         }
